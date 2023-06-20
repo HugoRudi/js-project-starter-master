4f2f547e6571b6bdc7194a99f8b2fca34bc00b89
Teko <carina.walter@edu.teko.ch>
2023-06-20 22:56:20 +0200

    Chat

--- client/script.js
+++ client/script.js
@@ -1,18 +1,52 @@
-const socket = new WebSocket("ws://localhost:3000");
+// script.js
 
-socket.addEventListener("open", (event) => {
-  console.log("WebSocket connected!");
-  socket.send("Hello, server!");
-});
+// Variablen für Benutzername und Nachricht erstellen
+let username = '';
+let message = '';
 
-socket.addEventListener("message", (event) => {
-  console.log(`Received message: ${event.data}`);
-});
+// Funktion für den Login
+function login() {
+  // Benutzernamen aus dem Eingabefeld abrufen
+  username = document.getElementById('username').value;
+  
+  // Überprüfen, ob ein Benutzername eingegeben wurde
+  if (username.trim() !== '') {
+    // Login-Sektion ausblenden
+    document.querySelector('.login-section').style.display = 'none';
+    
+    // Chat-Sektion anzeigen
+    document.querySelector('.chat-section').style.display = 'block';
+  }
+}
 
-socket.addEventListener("close", (event) => {
-  console.log("WebSocket closed.");
-});
+// Funktion zum Senden einer Nachricht
+function sendMessage() {
+  // Nachricht aus dem Eingabefeld abrufen
+  message = document.getElementById('message').value;
+  
+  // Überprüfen, ob eine Nachricht eingegeben wurde
+  if (message.trim() !== '') {
+    // Neue Nachricht erstellen und zum Nachrichtenfeld hinzufügen
+    const newMessage = document.createElement('div');
+    newMessage.classList.add('message');
 
-socket.addEventListener("error", (event) => {
-  console.error("WebSocket error:", event);
-});
+    // Benutzernamen zur Nachricht hinzufügen
+    const usernameElement = document.createElement('strong');
+    usernameElement.textContent = username;
+    newMessage.appendChild(usernameElement);
+
+    // Trennzeichen zur Nachricht hinzufügen
+    const separator = document.createTextNode(': ');
+    newMessage.appendChild(separator);
+
+    // Nachrichtentext zur Nachricht hinzufügen
+    const messageText = document.createTextNode(message);
+    newMessage.appendChild(messageText);
+
+    // Nachrichtenfeld aktualisieren
+    document.getElementById('message-box').appendChild(newMessage);
+    
+    // Nachrichtenfeld leeren
+    document.getElementById('message').value = '';
+  }
+}
